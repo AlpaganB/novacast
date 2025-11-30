82419ea0879da017d8b3f6db065c392a7fe1100b
Ömer Alpagan Barut <barutalpagan@gmail.com>
2025-11-30 23:21:02 +0300

    Update service worker cache version to v1.4.5

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'novacast-v1.4.4';
+const CACHE_NAME = 'novacast-v1.4.5';
 const urlsToCache = [
     './',
     './index.html',
@@ -90,4 +90,3 @@ self.addEventListener('push', event => {
         self.registration.showNotification('NovaCast Weather Alert', options)
     );
 });
-
